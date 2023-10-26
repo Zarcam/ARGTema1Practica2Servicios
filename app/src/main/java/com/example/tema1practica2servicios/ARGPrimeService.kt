b78b556eaf0850cd70bbdea68d5c15778339a556
zarcam <alejandrosatan.2896@gmail.com>
2023-10-26 20:30:43 +0200

    Ejercicio 1

---
+++ app/src/main/java/com/example/tema1practica2servicios/ARGPrimeService.kt
@@ -0,0 +1,11 @@
+package com.example.tema1practica2servicios
+
+import android.app.IntentService
+import android.content.Intent
+
+class ARGPrimeService : IntentService("ARGPrimeService") {
+    override fun onHandleIntent(intent: Intent?) {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/tema1practica2servicios/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.tema1practica2servicios
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private val MAYOR_INT: Int = Integer.MAX_VALUE/40000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.calcPrimosBoton).setOnClickListener{
+            var listaPrimos: ArrayList<Int> = ArrayList()
+            listaPrimos.addAll(calcPrimesFrom(MAYOR_INT))
+
+            listaPrimos.forEach {primo: Int ->
+                Log.d("Primos", primo.toString())
+            }
+        }
+
+        findViewById<Button>(R.id.cambiarColorBoton).setOnClickListener{
+            val color = Color.argb(255, Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+            it.setBackgroundColor(color)
+        }
+    }
+
+    private fun calcPrimesFrom(n: Int): List<Int>{
+        var primes = ArrayList<Int>()
+
+        for(i in 2..n){
+            if(isPrime(i, i-1)){
+                primes.add(i)
+            }
+        }
+        return primes
+    }
+
+    private fun isPrime(n: Int, div: Int): Boolean{
+        if(div == 1){
+            return true
+        }else if(n % div == 0){
+            return false
+        }else{
+            return isPrime(n, div-1)
+        }
+    }
+}
