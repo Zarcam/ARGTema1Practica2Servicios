5b9685041534eaf217f595f4eb784987c35a7259
zarcam <alejandrosatan.2896@gmail.com>
2023-10-31 18:14:14 +0100

    Preparando para crear servicio en primer plano

--- app/src/main/java/com/example/tema1practica2servicios/ARGPrimeService.kt
+++ app/src/main/java/com/example/tema1practica2servicios/ARGPrimeService.kt
@@ -1,11 +1,67 @@
 package com.example.tema1practica2servicios
 
-import android.app.IntentService
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
 import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
 
-class ARGPrimeService : IntentService("ARGPrimeService") {
-    override fun onHandleIntent(intent: Intent?) {
-
+class ARGPrimeService : Service() {
+    private val CHANNEL_ID = "ForegroundPrimeServiceChannel"
+    private lateinit var manager: NotificationManager
+    private var enable: Boolean = false
+    override fun onCreate() {
+        super.onCreate()
     }
 
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val serviceChannel = NotificationChannel(CHANNEL_ID, "Foreground Prime Service Channel", NotificationManager.IMPORTANCE_DEFAULT)
+            manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val input = intent.getStringExtra("inputExtra")
+        createNotificationChannel()
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Servicio calculando primos en primer plano")
+            .setContentText(input)
+            .setContentIntent(pendingIntent)
+            .build()
+        startForeground(102, notification)
+        this.run()
+        return START_NOT_STICKY
+    }
+
+    private fun run() {
+        enable = true
+        Thread{
+            while(enable){
+                Log.e("Hola", "Running", )
+                try{
+                    Thread.sleep(2000)
+                }catch (ex: InterruptedException){
+                    ex.printStackTrace()
+                }
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        enable = false
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
 }
--- app/src/main/java/com/example/tema1practica2servicios/MainActivity.kt
+++ app/src/main/java/com/example/tema1practica2servicios/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.tema1practica2servicios
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +18,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.calcPrimosBoton).setOnClickListener{
-            var listaPrimos: ArrayList<Int> = ArrayList()
+            /*var listaPrimos: ArrayList<Int> = ArrayList()
             listaPrimos.addAll(calcPrimesFrom(MAYOR_INT))
 
             listaPrimos.forEach {primo: Int ->
                 Log.d("Primos", primo.toString())
-            }
+            }*/
+            val serviceIntent = Intent(this, ARGPrimeService::class.java)
+            serviceIntent.putExtra("inputExtra", "Hola que tal")
+
+            ContextCompat.startForegroundService(this, serviceIntent)
         }
 
         findViewById<Button>(R.id.cambiarColorBoton).setOnClickListener{
